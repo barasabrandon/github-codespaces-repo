ad05768d5e5604a8ea8827d06d48aa2fff506ef4
barasabrandon <87303910+barasabrandon@users.noreply.github.com>
2023-10-16 18:10:41 +0000

    Worked on about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a web developer studying Electrical and Telecommunication Engineering at Moi University. I enjoy building simple but complex web apps that meet the needs which they are build.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "PHP/Laravel",
+  "React js",
+  "Firebase",
+  "MERN Stack",
+  "Flutter Mobile development",
 ];
 
 /**
